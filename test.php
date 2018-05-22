ba8e56c821f1809eaf3a9b005904f6663da8dcc8
liuyan <1260192349@qq.com>
2018-05-22 18:51:18 +0800

    test

--- test.php
+++ test.php
@@ -1,7 +1,7 @@
 <?php
 
    	$abc = ["http://images.yaxiangame.com/FrbetMHNRtlDIcTUVq7gwaHqPsQl"];
-    $a = json_encode(["abc"=>json_encode($abc)]);
+    $a = ["abc"=>json_encode($abc)];
 
     echo "<pre>";
     var_dump($a);die;

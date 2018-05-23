0a17b0a632c556894afcb0fb2e76c78e60184a90
liuyan <1260192349@qq.com>
2018-05-23 11:12:22 +0800

    tet

--- Tcp_demo.php
+++ Tcp_demo.php
@@ -1,7 +1,7 @@
 <?php
 // 创建TCP服务器
 
-$serv = new swoole_server("127.0.0.1","9501");
+$serv = new swoole_server("0.0.0.0","9501");
 
 $serv->on("connect",function($serv,$fd){
     echo "connect.\n";

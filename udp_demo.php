e9fb8d5c29bbae27b8d9ad36721ddcf363e21692
liuyan <1260192349@qq.com>
2018-05-24 11:01:01 +0800

    test

--- udp_demo.php
+++ udp_demo.php
@@ -1,5 +1,5 @@
 <?php
-	$serv = new swoole_server("127.0.0.1",9501,SWOOLE_PROCESS,SWOOLE_SOCK_UDP);
+	$serv = new swoole_server("0.0.0.0",9501,SWOOLE_PROCESS,SWOOLE_SOCK_UDP);
 
 	$serv->on("Packet",function($serv,$data,$clientInfo){
 		$serv->sendto($clientInfo['address'],$clientInfo['port'],"Server".$data);

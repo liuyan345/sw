c25224fdd0590f61dd3aad1f21e3fd8f9417e6c4
liuyan <1260192349@qq.com>
2018-05-24 14:29:17 +0800

    test

---
+++ websocket_demo.php
@@ -0,0 +1,21 @@
+<?php
+
+$ws = new swoole_websorcket_server("0.0.0.0",9501);
+
+$ws->on("open",function($ws,$request){
+	var_dump($request->fd,$request->get,$request->server);
+	$ws->push($request->fd,"hello,welcome\n");
+});
+
+$ws->on("message",function($ws,$frame){
+	echo "Message:{$frame->data}\n";
+	$ws->push($frame->fd,"server:{$frame->data}");
+});
+
+$ws->on("close",function($ws,$fd){
+	echo "client-{$fd} is closed\n";
+});
+
+$ws->start();
+
+?>

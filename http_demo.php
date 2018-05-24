44a3f731a2d48693e93018ac26605e4ce165335f
liuyan <1260192349@qq.com>
2018-05-24 11:35:37 +0800

    test

--- http_demo.php
+++ http_demo.php
@@ -3,6 +3,7 @@
 	$http = new swoole_http_server("0.0.0.0",9501);
 
 	$http->on("request",function($request,$response){
+		var_dump($request);
 		$response->header("Content-Type","text/html;charset=utf-8");
 		$response->end("<h1>Hello Swoole.#".rand(1000,9999)."</h1>");
 	});

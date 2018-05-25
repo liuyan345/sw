e6a47e6eb862836eb1acb005ae3cad6f138ad1bb
liuyan <1260192349@qq.com>
2018-05-25 09:59:07 +0800

    test

--- test.php
+++ test.php
@@ -1,9 +1,11 @@
 <?php
+swoole_timer_tick(2000, function ($timer_id) {
+    echo "tick-2000ms\n";
+});
+   	// $abc = ["http://images.yaxiangame.com/FrbetMHNRtlDIcTUVq7gwaHqPsQl"];
+    // $a = ["abc"=>json_encode($abc)];
 
-   	$abc = ["http://images.yaxiangame.com/FrbetMHNRtlDIcTUVq7gwaHqPsQl"];
-    $a = ["abc"=>json_encode($abc)];
-
-    echo "<pre>";
-    var_dump($a);die;
+    // echo "<pre>";
+    // var_dump($a);die;
 
 ?>

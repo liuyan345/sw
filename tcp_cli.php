fa6111d90e9a8c01e11be79075fbb673bcc9b247
liuyan <1260192349@qq.com>
2018-05-28 17:50:45 +0800

    test

---
+++ tcp_cli.php
@@ -0,0 +1,24 @@
+<?php
+	$serv = new swoole_server("0.0.0.0",9501);
+
+	$serv->set(array("task_worker_num"=>4));
+
+	$serv->on("receive",function($serv,$fd,$from_id,$data){
+		$task_id = $serv->task($data);
+		echo "Dispath AsyncTask:id=$task_id\n"；
+	}) ;
+
+	$serv->on("task",function($serv,$task_id,$from_id,$data){
+		echo "New AsyncTask[id=$task_id]".PHP_EOL;
+		$serv->finish("$data->OK");
+	});
+
+	$serv->on("finish",function($serv,$task_id,$data){
+		echo "AsyncTask[$task_id] Finish:$data".PHP_EOL;
+	});
+
+	$serv->start();
+
+
+
+?>

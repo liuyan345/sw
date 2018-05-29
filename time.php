1ff4a7277ed3cb870b834d357a1965cd6bfeee5f
liuyan <1260192349@qq.com>
2018-05-29 09:43:45 +0800

    tet

--- time.php
+++ time.php
@@ -8,14 +8,16 @@ swoole_timer_tick(2000,function($timer_id)use(&$i,&$timer){
 	echo $i."\n";
 });
 
-
-swoole_timer_after(10000,function()use($i,$timer){
-	echo "this is timer_after.\n";
-	var_dump($timer);
-	// if($i >= 4){
-		swoole_timer_clear($timer);
-	// }
-});
+echo "aaaaaaaaaaaaaaaaaaaa\n";
+var_dump($i);
+die;
+// swoole_timer_after(10000,function()use($i,$timer){
+// 	echo "this is timer_after.\n";
+// 	var_dump($timer);
+// 	// if($i >= 4){
+// 		swoole_timer_clear($timer);
+// 	// }
+// });
 
 
 

7580326eab909ae737f20c7d0c7748c4adb12418
liuyan <1260192349@qq.com>
2018-05-29 10:12:29 +0800

    ests

--- tcp_cli.php
+++ tcp_cli.php
@@ -5,7 +5,7 @@
 
 	$serv->on("receive",function($serv,$fd,$from_id,$data){
 		$task_id = $serv->task($data);
-		echo "Dispath AsyncTask:id=$task_id\n"；
+		echo "Dispath AsyncTask:id=$task_id\n";
 	}) ;
 
 	$serv->on("task",function($serv,$task_id,$from_id,$data){

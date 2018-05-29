ee726b518b0b3583153d0f76163b99375f1d9ae5
liuyan <1260192349@qq.com>
2018-05-29 09:25:28 +0800

    yrdy

---
+++ time.php
@@ -0,0 +1,18 @@
+<?php
+$i = 1;
+swoole_timer_tick(2000,function($timer_id){
+	var_dump($timer_id);
+	$i++;
+	echo $i."\n";
+	if($i == 4){
+		swoole_timer_clear($timer_id);
+	}
+});
+
+swoole_timer_after(10000,function(){
+	echo "this is timer_after.\n";
+});
+
+
+
+?>

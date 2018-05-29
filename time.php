804212c94a47d3f5b7aed920676c9ab744a41a4d
liuyan <1260192349@qq.com>
2018-05-29 09:42:11 +0800

    test

--- time.php
+++ time.php
@@ -9,10 +9,9 @@ swoole_timer_tick(2000,function($timer_id)use(&$i,&$timer){
 });
 
 
-swoole_timer_after(10000,function($timer_id)use($i,$timer){
+swoole_timer_after(10000,function()use($i,$timer){
 	echo "this is timer_after.\n";
 	var_dump($timer);
-	var_dump($timer_id);
 	// if($i >= 4){
 		swoole_timer_clear($timer);
 	// }

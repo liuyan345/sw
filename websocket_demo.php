ac7cd257fd1f874d4b7ba18bfceb702b385b5a7b
liuyan <1260192349@qq.com>
2019-03-04 16:13:56 +0800

    test

--- websocket_demo.php
+++ websocket_demo.php
@@ -1,25 +1,27 @@
 <?php
 
-//创建websocket服务器对象，监听0.0.0.0:9502端口
-$ws = new swoole_websocket_server("0.0.0.0", 9502);
+$ws = new swoole_websocket_server("0.0.0.0",9501);
 
-//监听WebSocket连接打开事件
-$ws->on('open', function ($ws, $request) {
-    var_dump($request->fd, $request->get, $request->server);
-    $ws->push($request->fd, "hello, welcome\n");
+$ws->on("open",function($ws,$request){
+	var_dump($request->fd,$request->get,$request->server);
+	$ws->push($request->fd,"hello,welcome\n");
 });
 
-//监听WebSocket消息事件
-$ws->on('message', function ($ws, $frame) {
-    echo "Message: {$frame->data}\n";
-    $ws->push($frame->fd, "server: {$frame->data}");
+$ws->on("message",function($ws,$frame){
+//	$i = 0;
+//	swoole_timer_tick(2000, function ($timer_id) {
+//		$i++;
+//		$ws->push($frame->fd,$i);
+//	});
+	 echo "Message:{$frame->data}\n";
 });
 
-//监听WebSocket连接关闭事件
-$ws->on('close', function ($ws, $fd) {
-    echo "client-{$fd} is closed\n";
+$ws->on("close",function($ws,$fd){
+	echo "client-{$fd} is closed\n";
 });
 
 $ws->start();
+
+
 
 ?>
